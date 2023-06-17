d1c063f5d34a3e751068464d08a6d53b5e70c6fd
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-17 00:03:33 -0700

    foundations of c writing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: the foundations of writing a c program
+ *Return: always 0 (sucess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
