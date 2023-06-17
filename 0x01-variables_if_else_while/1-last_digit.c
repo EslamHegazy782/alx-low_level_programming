a894e923cb5c02c54e6ad5f22e47e50d5556c1c2
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-17 09:07:50 -0700

    the value of last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,10 @@
 /**
  * main - entry point
  *
- * Description: A program to classify each number as greater or smaller than 5 or 0
+ * Description: A program print the relation of last number to 5,6&i
+ *		it shows if it is greaten than 5 or less than 6 or equal 0
  *
- * Return 0 (sucess)
+ * Return: 0 (sucess)
 */
 int main(void)
 {
@@ -14,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	digit = n % 10 ;
+	digit = n % 10;
 	if (digit > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
