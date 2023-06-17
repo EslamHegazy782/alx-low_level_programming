5e5e283793424fedc8bdf1a3d2b49104b7337877
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-17 04:14:44 -0700

    printing without printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: program used for printing
+ * Return: 1 (sucess)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", stdout);
+	return (1);
+}
