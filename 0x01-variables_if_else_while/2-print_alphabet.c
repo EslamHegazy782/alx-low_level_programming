52f45a9f7baa4bfb0f9b985d12d90c0ddce1f572
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-17 23:29:49 -0700

    printing lower letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Description: A program for printing all letters in lowercase
+ *
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
