d7419949949a11673e5592c00a1f1cbe955a888f
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-18 00:14:28 -0700

    lower and upper letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program for printing lower and uppercase letters
+ *
+ * Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	return (0);
+}
