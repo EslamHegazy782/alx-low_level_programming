392450a93c6ff93759665a4c3b4432c5938c8241
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-29 23:57:03 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - printing the absolute value
+ *
+ * @i: input as integar
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
