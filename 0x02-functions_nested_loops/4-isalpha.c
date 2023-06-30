180bd151501b9a14930991e99d96b0626788a865
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-30 00:22:52 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,9 @@
  * Return: 1  if c is a letter, lowercase or uppercase and
  *         0 for other.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (C >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
