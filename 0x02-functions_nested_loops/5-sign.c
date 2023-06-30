e32e4aba8969d61bc2af12ba08333129c9edec84
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-06-29 23:19:13 -0700

    sign of num.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of each input number.
+ *
+ * @n: The input number as integar
+ *
+ * Return: 1 if positive , 0 if zero , -1 if minus
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
