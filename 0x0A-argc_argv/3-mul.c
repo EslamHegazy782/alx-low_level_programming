941e732ecc40e90798eafd5ccab3bc084fc219a9
EslamHegazy782 <eslamhegazy782@gmail.com>
2023-10-06 08:00:20 -0700

    moly

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,9 +2,10 @@
 #include "main.h"
 
 /**
- *_atoi - convertsa string to an integer
- *@s: string to be converted
- *Return: the int converted from the string
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
@@ -17,6 +18,9 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 
+	while (s[len] != '\0')
+		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -26,33 +30,28 @@ int _atoi(char *s)
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-			{
 				digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
-			}
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
 				break;
 			f = 0;
-			}
 		}
 		i++;
 	}
 
 	if (f == 0)
-	{
 		return (0);
 
 	return (n);
-	}
 }
 
 /**
- *main - multiplies 2 no.
- *@argc: no of arguments
- *@argv: array of arguments
- *Return: 0 for success and 1 for error
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
@@ -60,7 +59,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 3 || argc > 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 
